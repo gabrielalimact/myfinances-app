a78f881861c7976e603b6ab71a0446f07e06efe1
Gabriela Lima <lgcl@ic.ufal.br>
2023-10-30 11:13:19 -0300

    register page

---
+++ app/register.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import { router } from 'expo-router'
+import { COLORS } from '../constants'
+import { Ionicons } from '@expo/vector-icons';
+import InputStyled from '../components/input';
+import BottomButtonsLogin from '../components/bottomButtonsLogin';
+
+
+const Register = () => {
+  const handleGoBack = () => {
+    router.back()
+  }
+
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
+        <View style={styles.container}>
+          <Ionicons name="ios-arrow-undo-outline" size={30} color="black" onPress={handleGoBack} />
+
+          <View style={styles.topArea}>
+            <Text style={styles.title}>Let's sign you up.</Text>
+            <Text style={styles.subtitle}>Welcome! Create an account to enjoy.</Text>
+          </View>
+
+          <View style={styles.inputView}>
+            <InputStyled label="Name" placeholder='Your name' />
+            <InputStyled label="Email" type='email-address' placeholder='Your email' />
+            <InputStyled label="Password" secureTextEntry={true} placeholder='Password'/>
+          </View>
+        </View>
+        
+        <BottomButtonsLogin isHome={true} buttonText='Sign up'/>
+    </SafeAreaView>
+  )
+}
+
+export default Register
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    gap: 10,
+  },
+  topArea: {
+    marginVertical: 20,
+
+  },
+  title: {
+    fontSize: 40,
+    fontFamily: 'medium',
+  },
+  subtitle: {
+    fontSize: 26,
+    fontFamily: 'regular',
+  },
+  inputView: {
+    gap: 20,
+  }
+})
